b41a611f6c256856f09193f55ac9e6e4775d385e
Ivens_Zhang <ivensz98@gmail.com>
2022-11-17 00:35:00 +1030

    feat: Add Bottom navigation component

---
+++ src/BottomNavigation/index.jsx
@@ -0,0 +1,10 @@
+
+const BottomNavigation = () => {
+  return (
+    <div className="flex justify-center w-full h-10 bg-blue-800 text-white">
+      BottomNavigation
+    </div>
+  )
+}
+
+export default BottomNavigation
